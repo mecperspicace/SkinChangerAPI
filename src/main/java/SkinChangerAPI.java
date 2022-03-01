d3e88611e9e0979841ef7ff3533c9a0fd763111d
BuildTools <gab.mercier51@gmail.com>
2022-03-01 11:23:38 +0100

    Initial Upload with ChangeSkin() in SkinChangerAPI.java

---
+++ src/main/java/SkinChangerAPI.java
@@ -0,0 +1,31 @@
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo;
+import net.minecraft.server.v1_8_R3.PlayerConnection;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkinChangerAPI extends JavaPlugin {
+
+    /**
+     * Change the player skin.
+     *
+     * @param player       The targeted player.
+     * @param skinProperty The property of the new player's skin.
+     */
+
+    public static void ChangePlayerSkin(Player player, Property skinProperty) {
+
+        //  Init the player's connection
+        GameProfile profile = ((CraftPlayer)player).getHandle().getProfile();
+        PlayerConnection connection = ((CraftPlayer)player).getHandle().playerConnection;
+
+        //  Send the packets
+        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, ((CraftPlayer)player).getHandle()));
+        profile.getProperties().removeAll("textures");
+        profile.getProperties().put("textures", skinProperty);
+        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, ((CraftPlayer)player).getHandle()));
+    }
+
+}
