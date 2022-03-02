5732124b37f4533fd59a1c82aa71b5fa7b14fbfc
BuildTools <gab.mercier51@gmail.com>
2022-03-02 18:20:49 +0100

    Add the getOnlinePlayerSkin() in SkinChangerAPI.java

--- src/main/java/fr/gabrielmercier/skinchangerapi/SkinChangerAPI.java
+++ src/main/java/fr/gabrielmercier/skinchangerapi/SkinChangerAPI.java
@@ -4,6 +4,7 @@ import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
+import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -28,6 +29,26 @@ public class SkinChangerAPI extends JavaPlugin {
         profile.getProperties().removeAll("textures");
         profile.getProperties().put("textures", skinProperty);
         connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, ((CraftPlayer)player).getHandle()));
+    }
+
+    /**
+     * Get the player's skin textures.
+     *
+     * @param playerName      The targeted player's name.
+     *
+     * @return playerTextures Return the player's skin textures.
+     */
+
+    public static Property GetOnlinePlayerSkin(String playerName){
+
+        // Init the player's connection
+        GameProfile profile = ((CraftPlayer)Bukkit.getPlayer(playerName)).getHandle().getProfile();
+
+        // Get the player's textures
+        Property playerTextures = profile.getProperties().get("textures").iterator().next();
+
+        // Return the player's textures
+        return playerTextures;
     }
 
 }
