54338ace207eb2dc2cceb96f852a5867a2d039d2
BuildTools <gab.mercier51@gmail.com>
2022-03-02 18:31:47 +0100

    Start the GetPlayerSkin() in SkinChangerAPI.java

--- src/main/java/fr/gabrielmercier/skinchangerapi/SkinChangerAPI.java
+++ src/main/java/fr/gabrielmercier/skinchangerapi/SkinChangerAPI.java
@@ -1,5 +1,7 @@
 package fr.gabrielmercier.skinchangerapi;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo;
@@ -8,6 +10,12 @@ import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
 
 public class SkinChangerAPI extends JavaPlugin {
 
@@ -49,6 +57,34 @@ public class SkinChangerAPI extends JavaPlugin {
 
         // Return the player's textures
         return playerTextures;
+    }
+
+    public static Property GetPlayerSkin(Player player) throws MalformedURLException {
+
+        // Init the request
+        UUID uuid = player.getUniqueId();
+        URL url = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid + "?unsigned=false");
+        Property playertextures = null;
+
+        // Do the request
+        try {
+
+            InputStreamReader reponse = new InputStreamReader(url.openStream());
+            JsonObject textureProperty = new JsonParser().parse(reponse).getAsJsonObject().get("properties").getAsJsonArray().get(0).getAsJsonObject();
+            String texture = textureProperty.get("value").getAsString();
+            String signature = textureProperty.get("signature").getAsString();
+
+            playertextures = new Property("textures", texture, signature);
+
+        } catch (IOException exception){
+
+            System.err.println(exception);
+
+        }
+
+        if (playertextures.hasSignature()) return playertextures;
+        else return null;
+
     }
 
 }
