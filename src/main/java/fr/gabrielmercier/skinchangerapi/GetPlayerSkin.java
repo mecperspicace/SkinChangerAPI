2ec6ec1ca73696759509049f29eab54995dab553
BuildTools <unconfigured@null.spigotmc.org>
2022-03-04 18:39:14 +0100

    Fix some bugs after the #df335e7

--- src/main/java/fr/gabrielmercier/skinchangerapi/GetPlayerSkin.java
+++ src/main/java/fr/gabrielmercier/skinchangerapi/GetPlayerSkin.java
@@ -13,6 +13,8 @@ import java.net.URL;
 
 public class GetPlayerSkin {
 
+    private Property defaultskin = new Property("textures", "eyJ0aW1lc3RhbXAiOjE1MTgzNDg5NzE5NjAsInByb2ZpbGVJZCI6IjcwOTU2NDU0NTJkOTRiYTI5YzcwZDFmYTY3YjhkYTQyIiwicHJvZmlsZU5hbWUiOiJIaWRkdXMiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzIxNTA3NjM2NDJmZjVjNmEzZjNhYzViNDlkYTZmN2NhNGQzZDliMTlmZjg2MjFkMTIwYTI1NWY4OWM5OTRiIn19fQ==", "OC0mYKDPjcVl6mynbt2r6PRsOotVpuOYHN/ovyALXkMIFnEqBpnhzQG2i+SZcFGUA+TXFbJ58QzaqOdEc3AUiRI3HRQ27amfpIq07gwxpwzhvk1BBTNxsP2Mw/5jw/+ttA0QDavm9OYid1aOzQLQ7WzgiWSUUucbTDyfu7pKSceERBvezRAqknZ6BiUgpCx4ahbiDbbHwDIA4N2HXEtK7TeqOuK2pyeICqopcu9d7W5ZUIw/tgUB04wq68FDV7kCrhAmlyl8Yn/PzkGeCk9dNbcuJVRRIu537FE+N/BrCmtNVqH7eQ6w1534rDVI7Q3jNDazT5HYeTNN9opkaMM02e9r/Nj7t3ZxNjhqCvRTVBr+IvrUMI/D164wei3iFVIjyFDNg9Pn5YsyW0sLibsDU0PJxU+SOsqDsJa9gATlxbx4eANBf9NF1O+J3j+Cob+00XdWlTMO/6+wQNDOoW9as9dKoSsxAzIZt/5U6iFioCoI8Lumy5DivBFZecUfCqxP9Fffal38uWlz5B2tkQTx3FFsROhpsAQcF6athaOA2nAfmuZhfe7Uik+ER13T8xWFPvfqjueC68pqDK4JIxNL0BwqdJb1UuZm1hFjdwDMuxsXfZcT4BZe/09Oja1rwChBFDYIMatg8to7u9yN+vqo3C9KmTDJgeIZ+VOjOFWBFzE=");
+
     /**
      * Change the player skin.
      *
@@ -21,8 +23,6 @@ public class GetPlayerSkin {
      */
 
     public Property byUUID(String uuid) {
-
-        Property playertextures = null;
 
         // Do the request
         try {
@@ -33,15 +33,14 @@ public class GetPlayerSkin {
             String texture = jsonproperty.get("value").getAsString();
             String signature = jsonproperty.get("signature").getAsString();
 
-            playertextures = new Property("textures", texture, signature);
+            return new Property("textures", texture, signature);
 
         } catch (IOException exception){
 
             System.err.println(exception);
-
+            return defaultskin;
         }
 
-        return playertextures;
     }
 
     /**
@@ -68,8 +67,6 @@ public class GetPlayerSkin {
 
         }
 
-        Property playertextures = null;
-
         // Do the second request
         try {
 
@@ -79,15 +76,14 @@ public class GetPlayerSkin {
             String texture = jsonproperty.get("value").getAsString();
             String signature = jsonproperty.get("signature").getAsString();
 
-            playertextures = new Property("textures", texture, signature);
+            return new Property("textures", texture, signature);
 
         } catch (IOException exception){
 
             System.err.println(exception);
+            return defaultskin;
 
         }
-
-        return playertextures;
 
     }
 
