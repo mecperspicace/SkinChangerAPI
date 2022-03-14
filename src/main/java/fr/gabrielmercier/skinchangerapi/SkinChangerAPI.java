c91559436a4d94ba5de17832b1f54e5348629d72
BuildTools <gab.mercier51@gmail.com>
2022-03-14 20:20:15 +0100

    Change the function name in SkinChangerAPI.java

--- src/main/java/fr/gabrielmercier/skinchangerapi/SkinChangerAPI.java
+++ src/main/java/fr/gabrielmercier/skinchangerapi/SkinChangerAPI.java
@@ -16,7 +16,7 @@ import java.net.URL;
 
 public class SkinChangerAPI extends JavaPlugin {
 
-    private static Property defaultskin = new Property("textures", "eyJ0aW1lc3RhbXAiOjE1MTgzNDg5NzE5NjAsInByb2ZpbGVJZCI6IjcwOTU2NDU0NTJkOTRiYTI5YzcwZDFmYTY3YjhkYTQyIiwicHJvZmlsZU5hbWUiOiJIaWRkdXMiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzIxNTA3NjM2NDJmZjVjNmEzZjNhYzViNDlkYTZmN2NhNGQzZDliMTlmZjg2MjFkMTIwYTI1NWY4OWM5OTRiIn19fQ==", "OC0mYKDPjcVl6mynbt2r6PRsOotVpuOYHN/ovyALXkMIFnEqBpnhzQG2i+SZcFGUA+TXFbJ58QzaqOdEc3AUiRI3HRQ27amfpIq07gwxpwzhvk1BBTNxsP2Mw/5jw/+ttA0QDavm9OYid1aOzQLQ7WzgiWSUUucbTDyfu7pKSceERBvezRAqknZ6BiUgpCx4ahbiDbbHwDIA4N2HXEtK7TeqOuK2pyeICqopcu9d7W5ZUIw/tgUB04wq68FDV7kCrhAmlyl8Yn/PzkGeCk9dNbcuJVRRIu537FE+N/BrCmtNVqH7eQ6w1534rDVI7Q3jNDazT5HYeTNN9opkaMM02e9r/Nj7t3ZxNjhqCvRTVBr+IvrUMI/D164wei3iFVIjyFDNg9Pn5YsyW0sLibsDU0PJxU+SOsqDsJa9gATlxbx4eANBf9NF1O+J3j+Cob+00XdWlTMO/6+wQNDOoW9as9dKoSsxAzIZt/5U6iFioCoI8Lumy5DivBFZecUfCqxP9Fffal38uWlz5B2tkQTx3FFsROhpsAQcF6athaOA2nAfmuZhfe7Uik+ER13T8xWFPvfqjueC68pqDK4JIxNL0BwqdJb1UuZm1hFjdwDMuxsXfZcT4BZe/09Oja1rwChBFDYIMatg8to7u9yN+vqo3C9KmTDJgeIZ+VOjOFWBFzE=");
+    private static Property DEFAULT_SKIN = new Property("textures", "eyJ0aW1lc3RhbXAiOjE1MTgzNDg5NzE5NjAsInByb2ZpbGVJZCI6IjcwOTU2NDU0NTJkOTRiYTI5YzcwZDFmYTY3YjhkYTQyIiwicHJvZmlsZU5hbWUiOiJIaWRkdXMiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzIxNTA3NjM2NDJmZjVjNmEzZjNhYzViNDlkYTZmN2NhNGQzZDliMTlmZjg2MjFkMTIwYTI1NWY4OWM5OTRiIn19fQ==", "OC0mYKDPjcVl6mynbt2r6PRsOotVpuOYHN/ovyALXkMIFnEqBpnhzQG2i+SZcFGUA+TXFbJ58QzaqOdEc3AUiRI3HRQ27amfpIq07gwxpwzhvk1BBTNxsP2Mw/5jw/+ttA0QDavm9OYid1aOzQLQ7WzgiWSUUucbTDyfu7pKSceERBvezRAqknZ6BiUgpCx4ahbiDbbHwDIA4N2HXEtK7TeqOuK2pyeICqopcu9d7W5ZUIw/tgUB04wq68FDV7kCrhAmlyl8Yn/PzkGeCk9dNbcuJVRRIu537FE+N/BrCmtNVqH7eQ6w1534rDVI7Q3jNDazT5HYeTNN9opkaMM02e9r/Nj7t3ZxNjhqCvRTVBr+IvrUMI/D164wei3iFVIjyFDNg9Pn5YsyW0sLibsDU0PJxU+SOsqDsJa9gATlxbx4eANBf9NF1O+J3j+Cob+00XdWlTMO/6+wQNDOoW9as9dKoSsxAzIZt/5U6iFioCoI8Lumy5DivBFZecUfCqxP9Fffal38uWlz5B2tkQTx3FFsROhpsAQcF6athaOA2nAfmuZhfe7Uik+ER13T8xWFPvfqjueC68pqDK4JIxNL0BwqdJb1UuZm1hFjdwDMuxsXfZcT4BZe/09Oja1rwChBFDYIMatg8to7u9yN+vqo3C9KmTDJgeIZ+VOjOFWBFzE=");
 
     /**
      * Change the player skin.
@@ -25,7 +25,7 @@ public class SkinChangerAPI extends JavaPlugin {
      * @param skinProperty The property of the new player's skin.
      */
 
-    public static void Change(Player player, Property skinProperty) {
+    public static void change(Player player, Property skinProperty) {
 
         //  Init the player's connection
         GameProfile profile = ((CraftPlayer)player).getHandle().getProfile();
@@ -45,7 +45,7 @@ public class SkinChangerAPI extends JavaPlugin {
      * @return playertextures The property of the player's skin.
      */
 
-    public static Property GetbyUUID(String uuid) {
+    public static Property getByUUID(String uuid) {
 
         // Do the request
         try {
@@ -61,7 +61,7 @@ public class SkinChangerAPI extends JavaPlugin {
         } catch (IOException exception){
 
             System.err.println(exception);
-            return defaultskin;
+            return DEFAULT_SKIN;
         }
 
     }
@@ -73,7 +73,7 @@ public class SkinChangerAPI extends JavaPlugin {
      * @return playertextures The property of the player's skin.
      */
 
-    public static Property GetbyName(String name) {
+    public static Property getByName(String name) {
 
         String uuid = "";
 
@@ -104,7 +104,7 @@ public class SkinChangerAPI extends JavaPlugin {
         } catch (IOException exception){
 
             System.err.println(exception);
-            return defaultskin;
+            return DEFAULT_SKIN;
 
         }
 
@@ -117,7 +117,7 @@ public class SkinChangerAPI extends JavaPlugin {
      * @return playertextures The property of the player's skin.
      */
 
-    public static Property GetbyObject(Player player){
+    public static Property getByObject(Player player){
 
         // Init the player's connection
         GameProfile profile = ((CraftPlayer) player).getHandle().getProfile();
