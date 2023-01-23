a659b1b5f1c04f8f62ead1313aa405e9951d5e4e
MΞCPERSPICACE <71231848+mecperspicace@users.noreply.github.com>
2023-01-23 10:33:43 +0000

    ✨ — Try to fix the player's name bug

--- src/main/java/fr/mecperspicace/skinchangerapi/SkinChangerAPI.java
+++ src/main/java/fr/mecperspicace/skinchangerapi/SkinChangerAPI.java
@@ -17,6 +17,7 @@ import java.net.URL;
 public class SkinChangerAPI extends JavaPlugin {
 
     private static Property DEFAULT_SKIN = new Property("textures", "eyJ0aW1lc3RhbXAiOjE1MTgzNDg5NzE5NjAsInByb2ZpbGVJZCI6IjcwOTU2NDU0NTJkOTRiYTI5YzcwZDFmYTY3YjhkYTQyIiwicHJvZmlsZU5hbWUiOiJIaWRkdXMiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzIxNTA3NjM2NDJmZjVjNmEzZjNhYzViNDlkYTZmN2NhNGQzZDliMTlmZjg2MjFkMTIwYTI1NWY4OWM5OTRiIn19fQ==", "OC0mYKDPjcVl6mynbt2r6PRsOotVpuOYHN/ovyALXkMIFnEqBpnhzQG2i+SZcFGUA+TXFbJ58QzaqOdEc3AUiRI3HRQ27amfpIq07gwxpwzhvk1BBTNxsP2Mw/5jw/+ttA0QDavm9OYid1aOzQLQ7WzgiWSUUucbTDyfu7pKSceERBvezRAqknZ6BiUgpCx4ahbiDbbHwDIA4N2HXEtK7TeqOuK2pyeICqopcu9d7W5ZUIw/tgUB04wq68FDV7kCrhAmlyl8Yn/PzkGeCk9dNbcuJVRRIu537FE+N/BrCmtNVqH7eQ6w1534rDVI7Q3jNDazT5HYeTNN9opkaMM02e9r/Nj7t3ZxNjhqCvRTVBr+IvrUMI/D164wei3iFVIjyFDNg9Pn5YsyW0sLibsDU0PJxU+SOsqDsJa9gATlxbx4eANBf9NF1O+J3j+Cob+00XdWlTMO/6+wQNDOoW9as9dKoSsxAzIZt/5U6iFioCoI8Lumy5DivBFZecUfCqxP9Fffal38uWlz5B2tkQTx3FFsROhpsAQcF6athaOA2nAfmuZhfe7Uik+ER13T8xWFPvfqjueC68pqDK4JIxNL0BwqdJb1UuZm1hFjdwDMuxsXfZcT4BZe/09Oja1rwChBFDYIMatg8to7u9yN+vqo3C9KmTDJgeIZ+VOjOFWBFzE=");
+    private static string error = "";
 
     /**
      * Change the player skin.
@@ -62,8 +63,9 @@ public class SkinChangerAPI extends JavaPlugin {
             return new Property("textures", texture, signature);
 
         } catch (IOException exception){
-
-            System.err.println(exception);
+            JsonObject jsonObject = new JsonObject(reponse);
+            error = reponse.getString("errorMessage");
+            Bukkit.getLogger().warn("Error while fetching player's skin with UUID...", exception);
             return DEFAULT_SKIN;
         }
 
@@ -91,8 +93,10 @@ public class SkinChangerAPI extends JavaPlugin {
             uuid = new JsonParser().parse(reponse1).getAsJsonObject().get("id").getAsString();
 
         } catch (IOException exception){
-
-            System.err.println(exception);
+            JsonObject jsonObject = new JsonObject(reponse);
+            error = reponse.getString("error");
+            Bukkit.getLogger().warn("Error while fetching player's skin with name...", error);
+            return DEFAULT_SKIN;
 
         }
 
@@ -101,9 +105,7 @@ public class SkinChangerAPI extends JavaPlugin {
 
             URL url2 = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid + "?unsigned=false");
             InputStreamReader reponse2 = new InputStreamReader(url2.openStream());
-            if (reponse2 == null) {
-                return DEFAULT_SKIN;
-            }
+
             JsonObject jsonproperty = new JsonParser().parse(reponse2).getAsJsonObject().get("properties").getAsJsonArray().get(0).getAsJsonObject();
             String texture = jsonproperty.get("value").getAsString();
             String signature = jsonproperty.get("signature").getAsString();
@@ -111,8 +113,9 @@ public class SkinChangerAPI extends JavaPlugin {
             return new Property("textures", texture, signature);
 
         } catch (IOException exception){
-
-            System.err.println(exception);
+            JsonObject jsonObject = new JsonObject(reponse);
+            error = reponse.getString("errorMessage");
+            Bukkit.getLogger().warn("Error while fetching player's skin with name...", error);
             return DEFAULT_SKIN;
 
         }
