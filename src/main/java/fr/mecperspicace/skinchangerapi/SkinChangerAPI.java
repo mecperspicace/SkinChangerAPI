c39453edb1d8bbc4d164f00d561984ff0f5332ec
Haniel Fialho <Hanielcota@hotmail.com>
2023-01-27 16:09:47 -0300

    It is important to note that these operations must be done inside the try-catch block to ensure that they are executed even if there is an exception.

--- src/main/java/fr/mecperspicace/skinchangerapi/SkinChangerAPI.java
+++ src/main/java/fr/mecperspicace/skinchangerapi/SkinChangerAPI.java
@@ -4,20 +4,25 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
+import net.minecraft.server.v1_8_R3.EntityPlayer;
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerInfo;
 import net.minecraft.server.v1_8_R3.PlayerConnection;
+import net.minecraft.server.v1_8_R3.WorldServer;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_8_R3.CraftServer;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class SkinChangerAPI extends JavaPlugin {
+public class SkinChangerAPI {
+    private static final Property DEFAULT_SKIN = new Property("textures", "eyJ0aW1lc3RhbXAiOjE1MTgzNDg5NzE5NjAsInByb2ZpbGVJZCI6IjcwOTU2NDU0NTJkOTRiYTI5YzcwZDFmYTY3YjhkYTQyIiwicHJvZmlsZU5hbWUiOiJIaWRkdXMiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzIxNTA3NjM2NDJmZjVjNmEzZjNhYzViNDlkYTZmN2NhNGQzZDliMTlmZjg2MjFkMTIwYTI1NWY4OWM5OTRiIn19fQ==", "OC0mYKDPjcVl6mynbt2r6PRsOotVpuOYHN/ovyALXkMIFnEqBpnhzQG2i+SZcFGUA+TXFbJ58QzaqOdEc3AUiRI3HRQ27amfpIq07gwxpwzhvk1BBTNxsP2Mw/5jw/+ttA0QDavm9OYid1aOzQLQ7WzgiWSUUucbTDyfu7pKSceERBvezRAqknZ6BiUgpCx4ahbiDbbHwDIA4N2HXEtK7TeqOuK2pyeICqopcu9d7W5ZUIw/tgUB04wq68FDV7kCrhAmlyl8Yn/PzkGeCk9dNbcuJVRRIu537FE+N/BrCmtNVqH7eQ6w1534rDVI7Q3jNDazT5HYeTNN9opkaMM02e9r/Nj7t3ZxNjhqCvRTVBr+IvrUMI/D164wei3iFVIjyFDNg9Pn5YsyW0sLibsDU0PJxU+SOsqDsJa9gATlxbx4eANBf9NF1O+J3j+Cob+00XdWlTMO/6+wQNDOoW9as9dKoSsxAzIZt/5U6iFioCoI8Lumy5DivBFZecUfCqxP9Fffal38uWlz5B2tkQTx3FFsROhpsAQcF6athaOA2nAfmuZhfe7Uik+ER13T8xWFPvfqjueC68pqDK4JIxNL0BwqdJb1UuZm1hFjdwDMuxsXfZcT4BZe/09Oja1rwChBFDYIMatg8to7u9yN+vqo3C9KmTDJgeIZ+VOjOFWBFzE=");
 
-    private static Property DEFAULT_SKIN = new Property("textures", "eyJ0aW1lc3RhbXAiOjE1MTgzNDg5NzE5NjAsInByb2ZpbGVJZCI6IjcwOTU2NDU0NTJkOTRiYTI5YzcwZDFmYTY3YjhkYTQyIiwicHJvZmlsZU5hbWUiOiJIaWRkdXMiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzIxNTA3NjM2NDJmZjVjNmEzZjNhYzViNDlkYTZmN2NhNGQzZDliMTlmZjg2MjFkMTIwYTI1NWY4OWM5OTRiIn19fQ==", "OC0mYKDPjcVl6mynbt2r6PRsOotVpuOYHN/ovyALXkMIFnEqBpnhzQG2i+SZcFGUA+TXFbJ58QzaqOdEc3AUiRI3HRQ27amfpIq07gwxpwzhvk1BBTNxsP2Mw/5jw/+ttA0QDavm9OYid1aOzQLQ7WzgiWSUUucbTDyfu7pKSceERBvezRAqknZ6BiUgpCx4ahbiDbbHwDIA4N2HXEtK7TeqOuK2pyeICqopcu9d7W5ZUIw/tgUB04wq68FDV7kCrhAmlyl8Yn/PzkGeCk9dNbcuJVRRIu537FE+N/BrCmtNVqH7eQ6w1534rDVI7Q3jNDazT5HYeTNN9opkaMM02e9r/Nj7t3ZxNjhqCvRTVBr+IvrUMI/D164wei3iFVIjyFDNg9Pn5YsyW0sLibsDU0PJxU+SOsqDsJa9gATlxbx4eANBf9NF1O+J3j+Cob+00XdWlTMO/6+wQNDOoW9as9dKoSsxAzIZt/5U6iFioCoI8Lumy5DivBFZecUfCqxP9Fffal38uWlz5B2tkQTx3FFsROhpsAQcF6athaOA2nAfmuZhfe7Uik+ER13T8xWFPvfqjueC68pqDK4JIxNL0BwqdJb1UuZm1hFjdwDMuxsXfZcT4BZe/09Oja1rwChBFDYIMatg8to7u9yN+vqo3C9KmTDJgeIZ+VOjOFWBFzE=");
-    private static string error = "";
 
     /**
      * Change the player skin.
@@ -29,30 +34,52 @@ public class SkinChangerAPI extends JavaPlugin {
     public static void change(Player player, Property skinProperty) {
 
         //  Init the player's connection
-        GameProfile profile = ((CraftPlayer)player).getHandle().getProfile();
-        PlayerConnection connection = ((CraftPlayer)player).getHandle().playerConnection;
+        GameProfile profile = ((CraftPlayer) player).getHandle().getProfile();
+        PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
 
         //  Send the packets
-        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, ((CraftPlayer)player).getHandle()));
+        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.REMOVE_PLAYER, ((CraftPlayer) player).getHandle()));
         profile.getProperties().removeAll("textures");
         profile.getProperties().put("textures", skinProperty);
-        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, ((CraftPlayer)player).getHandle()));
+        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, ((CraftPlayer) player).getHandle()));
+        updateSkinWhitRelog(player);
+        for (Player on : Bukkit.getOnlinePlayers()) {
+            on.hidePlayer(player);
+            on.showPlayer(player);
+        }
+    }
+
+    public static void updateSkinWhitRelog(Player player) {
+        EntityPlayer ep = ((CraftPlayer) player).getHandle();
+        CraftWorld cw = (CraftWorld) player.getWorld();
+        WorldServer ws = cw.getHandle();
+        int dimension = ws.dimension;
+        ((CraftServer) Bukkit.getServer()).getHandle().moveToWorld(ep, dimension, true, player.getLocation(), true);
     }
 
     /**
      * Get the player skin.
      *
-     * @param uuid            The targeted player's uuid.
+     * @param uuid The targeted player's uuid.
      * @return playertextures The property of the player's skin.
      */
 
     public static Property getByUUID(String uuid) {
-
         // Do the request
         try {
 
             URL url = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid + "?unsigned=false");
-            InputStreamReader reponse = new InputStreamReader(url.openStream());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            if (connection.getResponseCode() != 200) {
+                return DEFAULT_SKIN;
+            }
+
+            InputStream input = connection.getInputStream();
+            InputStreamReader reponse = new InputStreamReader(input);
             if (reponse == null) {
                 return DEFAULT_SKIN;
             }
@@ -60,21 +87,25 @@ public class SkinChangerAPI extends JavaPlugin {
             String texture = jsonproperty.get("value").getAsString();
             String signature = jsonproperty.get("signature").getAsString();
 
+            reponse.close();
+            input.close();
+            connection.disconnect();
+
             return new Property("textures", texture, signature);
 
-        } catch (IOException exception){
-            JsonObject jsonObject = new JsonObject(reponse);
-            error = reponse.getString("errorMessage");
-            Bukkit.getLogger().warn("Error while fetching player's skin with UUID...", exception);
+        } catch (IOException exception) {
+
+            System.err.println(exception);
             return DEFAULT_SKIN;
         }
 
     }
 
+
     /**
      * Get the player skin.
      *
-     * @param name            The targeted player's name.
+     * @param name The targeted player's name.
      * @return playertextures The property of the player's skin.
      */
 
@@ -84,52 +115,60 @@ public class SkinChangerAPI extends JavaPlugin {
 
         // Do the first request
         try {
-
             URL url1 = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
-            InputStreamReader reponse1 = new InputStreamReader(url1.openStream());
-            if (reponse1 == null) {
+            HttpURLConnection connection = (HttpURLConnection) url1.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            if (connection.getResponseCode() != 200) {
                 return DEFAULT_SKIN;
             }
+
+            InputStreamReader reponse1 = new InputStreamReader(connection.getInputStream());
             uuid = new JsonParser().parse(reponse1).getAsJsonObject().get("id").getAsString();
-
-        } catch (IOException exception){
-            JsonObject jsonObject = new JsonObject(reponse);
-            error = reponse.getString("error");
-            Bukkit.getLogger().warn("Error while fetching player's skin with name...", error);
+            reponse1.close();
+        } catch (IOException exception) {
+            System.err.println(exception);
             return DEFAULT_SKIN;
-
         }
 
         // Do the second request
         try {
 
             URL url2 = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid + "?unsigned=false");
-            InputStreamReader reponse2 = new InputStreamReader(url2.openStream());
-
+            HttpURLConnection connection = (HttpURLConnection) url2.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            if (connection.getResponseCode() != 200) {
+                return DEFAULT_SKIN;
+            }
+            InputStreamReader reponse2 = new InputStreamReader(connection.getInputStream());
             JsonObject jsonproperty = new JsonParser().parse(reponse2).getAsJsonObject().get("properties").getAsJsonArray().get(0).getAsJsonObject();
             String texture = jsonproperty.get("value").getAsString();
             String signature = jsonproperty.get("signature").getAsString();
 
             return new Property("textures", texture, signature);
 
-        } catch (IOException exception){
-            JsonObject jsonObject = new JsonObject(reponse);
-            error = reponse.getString("errorMessage");
-            Bukkit.getLogger().warn("Error while fetching player's skin with name...", error);
+        } catch (IOException exception) {
+
+            System.err.println(exception);
             return DEFAULT_SKIN;
 
         }
 
     }
 
+
     /**
      * Get the player skin.
      *
-     * @param player          The targeted player's uuid.
+     * @param player The targeted player's uuid.
      * @return playertextures The property of the player's skin.
      */
 
-    public static Property getByObject(Player player){
+    public static Property getByObject(Player player) {
 
         // Init the player's connection
         GameProfile profile = ((CraftPlayer) player).getHandle().getProfile();
